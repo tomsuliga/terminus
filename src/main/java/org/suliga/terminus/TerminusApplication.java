7cde53f18e9ff452ee4ae29cc58e55ce9afc2f5a
tom <tom2wheeler@gmail.com>
2017-01-12 07:27:17 -0700

    Initial commit of base spring boot project

---
+++ src/main/java/org/suliga/terminus/TerminusApplication.java
@@ -0,0 +1,12 @@
+package org.suliga.terminus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerminusApplication.class, args);
+	}
+}
